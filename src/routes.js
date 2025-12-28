ace696c39a0091ed652c35f9bafb56e396d75217
Najme-Mohammadi <najme.devacc@gmail.com>
2025-12-28 12:07:24 +0330

    feature(router): add main router with auth, product, category and cart modules

--- src/routes.js
+++ src/routes.js
@@ -1,8 +1,8 @@
 import express from "express";
 import authRoutes from "./modules/auth/auth.routes.js";
-import productRoutes from "./modules/product/product.routes.js";
-import categoryRoutes from "./modules/category/category.routes.js";
-import cartRoutes from "./modules/cart/cart.routes.js";
+import productRoutes from "./modules/products/product.routes.js";
+import categoryRoutes from "./modules/categories/category.routes.js";
+import cartRoutes from "./modules/carts/cart.routes.js";
 
 const router = express.Router();
 
@@ -13,20 +13,3 @@ router.use("/cart", cartRoutes);
 
 export default router;
 
-// import express from "express";
-// const router = express.Router();
-
-// const authRoutes = require('./modules/auth/auth.routes');
-// router.use('/auth', authRoutes);
-// // console.log(typeof authRoutes);
-
-// const productRoutes = require('./modules/product/product.routes');
-// router.use('/product', productRoutes);
-
-// const categoryRoutes = require('./modules/category/category.routes');
-// router.use('/product', categoryRoutes);
-
-// const cartRoutes = require('./modules/cart/cart.routes');
-// router.use('/cart', cartRoutes);
-
-// export default router;
