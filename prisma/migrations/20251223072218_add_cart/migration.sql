7321d009056005731d1d00843f5363d38b0f4788
Najme-Mohammadi <najme.devacc@gmail.com>
2025-12-28 12:00:07 +0330

    db(migration): add initial migrations and update schema

---
+++ prisma/migrations/20251223072218_add_cart/migration.sql
@@ -0,0 +1,31 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `status` on the `Cart` table. All the data in the column will be lost.
+  - A unique constraint covering the columns `[userId]` on the table `Cart` will be added. If there are existing duplicate values, this will fail.
+  - A unique constraint covering the columns `[cartId,productId]` on the table `CartItem` will be added. If there are existing duplicate values, this will fail.
+
+*/
+-- DropForeignKey
+ALTER TABLE "Cart" DROP CONSTRAINT "Cart_userId_fkey";
+
+-- DropForeignKey
+ALTER TABLE "CartItem" DROP CONSTRAINT "CartItem_cartId_fkey";
+
+-- AlterTable
+ALTER TABLE "Cart" DROP COLUMN "status";
+
+-- AlterTable
+ALTER TABLE "CartItem" ALTER COLUMN "quantity" SET DEFAULT 1;
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Cart_userId_key" ON "Cart"("userId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "CartItem_cartId_productId_key" ON "CartItem"("cartId", "productId");
+
+-- AddForeignKey
+ALTER TABLE "Cart" ADD CONSTRAINT "Cart_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "CartItem" ADD CONSTRAINT "CartItem_cartId_fkey" FOREIGN KEY ("cartId") REFERENCES "Cart"("id") ON DELETE CASCADE ON UPDATE CASCADE;
